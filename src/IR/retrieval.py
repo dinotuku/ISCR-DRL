385eb0017f1e06a6b121d96af78f15778d533447
dinotuku <tungkuan0528@gmail.com>
2018-06-11 11:08:02 +0800

    Change back to cross entropy

--- src/IR/retrieval.py
+++ src/IR/retrieval.py
@@ -6,8 +6,6 @@ from util import *
 from retmath import *
 from copy import *
 
-from sklearn.metrics.pairwise import cosine_similarity
-
 
 def retrieveCombination(query, negquery, background, inv_index, docleng, alpha, beta):
     result = {}
@@ -18,50 +16,46 @@ def retrieveCombination(query, negquery, background, inv_index, docleng, alpha, 
             for docID, val in inv_index[wordID].iteritems():
                 existDoc[docID] = 1
                 # smooth doc model by background
-                # alpha_d = docleng[docID] / (docleng[docID] + alpha)
-                # docprob = (1 - alpha_d) * background[wordID] + alpha_d * val
-                docprob = val
+                alpha_d = docleng[docID] / (docleng[docID] + alpha)
+                docprob = (1 - alpha_d) * background[wordID] + alpha_d * val
                 qryprob = weight
                 if result[docID] != -9999:
-                    result[docID] += cosine_similarity(qryprob, docprob)
+                    result[docID] += cross_entropy(qryprob, docprob)
                 else:
-                    result[docID] = cosine_similarity(qryprob, docprob)
+                    result[docID] = cross_entropy(qryprob, docprob)
 
             for docID, val in result.iteritems():
                 if not existDoc.has_key(docID) and background.has_key(wordID):
-                    # alpha_d = docleng[docID] / (docleng[docID] + alpha)
-                    # docprob = (1 - alpha_d) * background[wordID]
-                    docprob = val
+                    alpha_d = docleng[docID] / (docleng[docID] + alpha)
+                    docprob = (1 - alpha_d) * background[wordID]
                     qryprob = weight
                     if result[docID] != -9999:
-                        result[docID] += cosine_similarity(qryprob, docprob)
+                        result[docID] += cross_entropy(qryprob, docprob)
                     else:
-                        result[docID] = cosine_similarity(qryprob, docprob)
+                        result[docID] = cross_entropy(qryprob, docprob)
 
         for wordID, weight in negquery.iteritems():
             existDoc = {}
             for docID, val in inv_index[wordID].iteritems():
                 existDoc[docID] = 1
                 # smooth doc model by background
-                # alpha_d = docleng[docID] / (docleng[docID] + alpha)
-                # docprob = (1 - alpha_d) * background[wordID] + alpha_d * val
-                docprob = val
+                alpha_d = docleng[docID] / (docleng[docID] + alpha)
+                docprob = (1 - alpha_d) * background[wordID] + alpha_d * val
                 qryprob = weight
                 if result[docID] != -9999:
-                    result[docID] -= beta * cosine_similarity(qryprob, docprob)
+                    result[docID] -= beta * cross_entropy(qryprob, docprob)
                 else:
-                    result[docID] = -1 * beta * cosine_similarity(qryprob, docprob)
+                    result[docID] = -1 * beta * cross_entropy(qryprob, docprob)
 
             for docID, val in result.iteritems():
                 if not existDoc.has_key(docID) and background.has_key(wordID):
-                    # alpha_d = docleng[docID] / (docleng[docID] + alpha)
-                    # docprob = (1 - alpha_d) * background[wordID]
-                    docprob = val
+                    alpha_d = docleng[docID] / (docleng[docID] + alpha)
+                    docprob = (1 - alpha_d) * background[wordID]
                     qryprob = weight
                     if result[docID] != -9999:
-                        result[docID] -= beta * cosine_similarity(qryprob, docprob)
+                        result[docID] -= beta * cross_entropy(qryprob, docprob)
                     else:
-                        result[docID] = -1 * beta * cosine_similarity(qryprob, docprob)
+                        result[docID] = -1 * beta * cross_entropy(qryprob, docprob)
 
     sorted_ret = sorted(result.iteritems(), key=operator.itemgetter(1),reverse=True)	
     return sorted_ret
